b0c5bc49ad8a5fcd900209ae69110c9160283186
Lucas <lucasferraz11@hotmail.com>
2014-05-20 22:03:15 -0300

    Sexto Commit: Adicionado Classes para as Telas.

---
+++ InfracoesUrbanas/src/com/tcc/infracoesurbanas/Inquiry.java
@@ -0,0 +1,14 @@
+package com.tcc.infracoesurbanas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Inquiry extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inquiry);
+	}
+	
+}
---
+++ InfracoesUrbanas/src/com/tcc/infracoesurbanas/Menu.java
@@ -0,0 +1,27 @@
+package com.tcc.infracoesurbanas;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Menu extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+	}
+	
+	public void onToCadastreButtonClick(View v){
+		Intent i = new Intent();
+		i.setClass(this, Cadastre.class);
+		startActivity(i);
+	}
+	
+	public void onToInquiryButtonClick(View v){
+		Intent i = new Intent();
+		i.setClass(this, Inquiry.class);
+		startActivity(i);
+	}
+}
