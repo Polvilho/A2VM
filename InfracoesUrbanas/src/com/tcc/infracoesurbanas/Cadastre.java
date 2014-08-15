f8f3bd08de6f69b01dc5a67c7123dc21990a6247
Lucas <lucasferraz11@hotmail.com>
2014-08-15 16:09:51 -0300

    Oitavo Commit: Retirado JARS desnecessários e bugs para launch da app reparados

--- InfracoesUrbanas/src/com/tcc/infracoesurbanas/Cadastre.java
+++ InfracoesUrbanas/src/com/tcc/infracoesurbanas/Cadastre.java
@@ -1,17 +1,11 @@
 package com.tcc.infracoesurbanas;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-
-
-
+import android.view.View;
 
 public class Cadastre extends Activity{
-	
-	private String id;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -19,19 +13,10 @@ public class Cadastre extends Activity{
 		setContentView(R.layout.cadastre);
 	}
 	
-	EditText editTextIDInfracao = (EditText) findViewById(R.id.infractionNumberText);
-	EditText editTextPlaca = (EditText) findViewById(R.id.boardEditTextCadastre);
-	EditText editTextName = (EditText) findViewById(R.id.textName);
-	EditText editTextData = (EditText) findViewById(R.id.TextDate);
-	EditText editTextHora = (EditText) findViewById(R.id.TextHour);
-	Spinner spinnerOrgao = (Spinner) findViewById(R.id.orgaoSpinner);
-	Spinner spinnerMarca = (Spinner) findViewById(R.id.companySpinner);
-	Spinner spinnerAno = (Spinner) findViewById(R.id.yearSpinner);
-	Button buttonCadastrar = (Button) findViewById(R.id.cadastreButton);
-	Button buttonLimpar = (Button) findViewById(R.id.cleanButton);
-	Button buttonCapturar = (Button) findViewById(R.id.captureButton);
-	Button buttonBackToMenu = (Button) findViewById(R.id.buttonBackToMenu);
-	
-	
-	
+	public void onBackToMenuButtonClick(View v) {
+		Intent i = new Intent();
+		i.setClass(this, Menu.class);
+		startActivity(i);
+	}
+
 }
